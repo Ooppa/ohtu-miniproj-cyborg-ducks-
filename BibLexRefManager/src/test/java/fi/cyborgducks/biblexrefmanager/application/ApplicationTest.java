ee361a5906b77454995a8337d960eb940a48e782
Ooppa <OpaPosti@hotmail.com>
2015-03-30 20:18:13 +0300

    Travis CI geneerinen testi

---
+++ BibLexRefManager/src/test/java/fi/cyborgducks/biblexrefmanager/application/ApplicationTest.java
@@ -0,0 +1,32 @@
+/*
+ * Miniprojekti Ohjelmistotuotanto kevät 2015
+ * Helsingin yliopisto Tietojenkäsittelytieteen laitos
+ * Cyborg Ducks 2015 - GNU General Public License, version 3.
+ */
+package fi.cyborgducks.biblexrefmanager.application;
+
+import org.junit.*;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Generic test-class to try out Travis CI.
+ */
+public class ApplicationTest {
+
+    public ApplicationTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGeneric() {
+        assertTrue(true);
+    }
+
+}
