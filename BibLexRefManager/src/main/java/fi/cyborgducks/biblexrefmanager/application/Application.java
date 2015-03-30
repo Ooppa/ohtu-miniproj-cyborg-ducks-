4887554dcff1d912b91b1f46a4da5643e21f3884
Ooppa <OpaPosti@hotmail.com>
2015-03-30 19:39:52 +0300

    Netbeans projektipohja

---
+++ BibLexRefManager/src/main/java/fi/cyborgducks/biblexrefmanager/application/Application.java
@@ -0,0 +1,16 @@
+/*
+ * Miniprojekti Ohjelmistotuotanto kevät 2015
+ * Helsingin yliopisto Tietojenkäsittelytieteen laitos
+ * Cyborg Ducks 2015 - GNU General Public License, version 3.
+ */
+package fi.cyborgducks.biblexrefmanager.application;
+
+/**
+ * Starting point for the application.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
