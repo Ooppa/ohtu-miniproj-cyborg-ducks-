28000c6db7188ed0c22789ca633451852da920e0
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-04-09 15:17:20 +0300

    fixed database tests after update to book

--- BibLexRefManager/src/test/java/fi/cyborgducks/biblexrefmanager/data/InMemoryDatabaseTest.java
+++ BibLexRefManager/src/test/java/fi/cyborgducks/biblexrefmanager/data/InMemoryDatabaseTest.java
@@ -25,19 +25,36 @@ public class InMemoryDatabaseTest {
 
     @Test
     public void addedReferenceIncreasesCountAtDatabase() {
-        Reference r = new Book("123", "Samu", "Samus publisher", "2015");
+        Reference r = new Book("123", "Samu", "Super book", "Samus publisher", "2015");
         imd.saveReference(r);
         assertEquals(1, imd.getAllSavedReferences().size());
     }
 
     @Test
-    public void multipleAddedReferencesIncreasesCountProperly() {
-        Reference r = new Book("123", "Samu", "Samus publisher", "2015");
-        Reference r2 = new Book("321", "Peelo", "Peelos publisher", "2012");
+    public void twoAddedReferencesIncreasesCountProperly() {
+        Reference r = new Book("123", "Samu", "Super book", "Samus publisher", "2015");
+        Reference r2 = new Book("321", "Peelo", "Super book", "Peelos publisher", "2012");
         imd.saveReference(r);
         imd.saveReference(r2);
         assertEquals(2, imd.getAllSavedReferences().size());
 
+    }
+
+    @Test
+    public void multipleAddedReferencesIncreasesCountProperly() {
+        for (int i = 0; i < 5; i++) {
+            Reference r = new Book("123" + i, "Samu" + i, "Super Book " + i, "Samus publisher" + i, "2015" + i);
+            imd.saveReference(r);
+        }
+        assertEquals(5, imd.getAllSavedReferences().size());
+    }
+
+    @Test
+    public void dataOfTheReferenceIsRightWhenFetched() {
+        Reference r = new Book("123", "Samu", "Super Book", "Samus publisher", "2015");
+        imd.saveReference(r);
+        Reference fetched = imd.getAllSavedReferences().get(0);
+        assertEquals(true, fetched.isComplete());
     }
 
 }
