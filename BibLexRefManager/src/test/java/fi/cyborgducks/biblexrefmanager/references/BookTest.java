1e4c16d006f6d04e4fe35d6e1ebc522ce5c6e367
Ooppa <OpaPosti@hotmail.com>
2015-04-09 15:23:16 +0300

    Book luokan yksikkötestit

---
+++ BibLexRefManager/src/test/java/fi/cyborgducks/biblexrefmanager/references/BookTest.java
@@ -0,0 +1,59 @@
+/*
+ * Miniprojekti Ohjelmistotuotanto kevät 2015
+ * Helsingin yliopisto Tietojenkäsittelytieteen laitos
+ * Cyborg Ducks 2015 - GNU General Public License, version 3.
+ */
+package fi.cyborgducks.biblexrefmanager.references;
+
+import org.jbibtex.BibTeXEntry;
+import org.jbibtex.KeyValue;
+import org.junit.*;
+import static org.junit.Assert.assertTrue;
+
+/**
+ *
+ * @author Ooppa
+ */
+public class BookTest {
+
+    private Book book;
+
+    public BookTest() {
+    }
+
+    @Before
+    public void setUp() {
+        book = new Book(
+                "cleancode",
+                "Robert C. Martin",
+                "Clean Code: A Handbook of Agile Software Craftsmanship",
+                "Prentice Hall",
+                "2008");
+    }
+
+    @After
+    public void tearDown() {
+        book = null;
+    }
+
+    @Test
+    public void testIsCompleteAfterCreation() {
+        assertTrue(book.isComplete());
+    }
+
+    @Test
+    public void testAbleToAddOptionalField() {
+        KeyValue value = new KeyValue("First Edition");
+        book.addField(BibTeXEntry.KEY_EDITION, value);
+        
+        assertTrue(book.getField(BibTeXEntry.KEY_EDITION).equals(value));
+    }
+    
+    @Test
+    public void testNotAbleToAddWrongField() {
+        book.addField(BibTeXEntry.KEY_HOWPUBLISHED, new KeyValue("Not like this!"));
+        
+        assertTrue(book.getField(BibTeXEntry.KEY_HOWPUBLISHED)==null);
+    }
+
+}
