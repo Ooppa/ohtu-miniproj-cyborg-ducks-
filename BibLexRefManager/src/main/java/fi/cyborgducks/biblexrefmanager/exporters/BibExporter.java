50b40ada682860f7e76d39d323a24d0da6180f64
Kristian W <kristian.wahlroos@gmail.com>
2015-04-11 20:39:49 +0300

    easyB tests

--- BibLexRefManager/src/main/java/fi/cyborgducks/biblexrefmanager/exporters/BibExporter.java
+++ BibLexRefManager/src/main/java/fi/cyborgducks/biblexrefmanager/exporters/BibExporter.java
@@ -17,7 +17,22 @@ public class BibExporter {
         String path = ChooseFile();
 
         Writer writer = new BufferedWriter(
-                new OutputStreamWriter(new FileOutputStream(path+".bib"), "8859_1")
+                new OutputStreamWriter(new FileOutputStream(path + ".bib"), "8859_1")
+        );
+
+        try {
+            BibTeXFormatter formatter = new BibTeXFormatter();
+            formatter.format(db, writer);
+        } finally {
+            writer.close();
+        }
+    }
+
+    public void exportStub(BibTeXDatabase db, String p) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+        String path = p;
+
+        Writer writer = new BufferedWriter(
+                new OutputStreamWriter(new FileOutputStream(path + ".bib"), "8859_1")
         );
 
         try {
@@ -30,17 +45,17 @@ public class BibExporter {
 
     private static String ChooseFile() {
         JFileChooser chooser = new JFileChooser();
-        
         FileNameExtensionFilter filter = new FileNameExtensionFilter("bib files", "bib");
         chooser.setFileFilter(filter);
-        
-        Component parent = new Component() { };
+
+        Component parent = new Component() {
+        };
         int returnVal = chooser.showOpenDialog(parent);
-        
-        if(returnVal==JFileChooser.APPROVE_OPTION) {
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
             return chooser.getSelectedFile().getAbsolutePath();
         }
-        
+
         return null;
     }
 }
--- BibLexRefManager/src/test/easyb/export_reference_Story.groovy
+++ BibLexRefManager/src/test/easyb/export_reference_Story.groovy
@@ -1,17 +1,64 @@
 import fi.cyborgducks.biblexrefmanager.*;
-
+import fi.cyborgducks.biblexrefmanager.data.*;
+import fi.cyborgducks.biblexrefmanager.references.*;
+import fi.cyborgducks.biblexrefmanager.exporters.*;
 
 
 description 'User can export all saved references'
 
-scenario "all exported book references can be found from the created file", {
-    given 'export as .bib'
-    when 'book references have been saved to database'
-    then 'all book references are being exported to .bib-file'
+scenario "new .bib file is created", {
+    given 'export as .bib', {
+        book = new Book(
+                "cleancode",
+                "Robert C. Martin",
+                "Clean Code: A Handbook of Agile Software Craftsmanship",
+                "Prentice Hall",
+                "2008")
+        database = new InMemoryDatabase()
+        database.saveReference(book)
+    }
+    when 'book references have been saved to database', {
+        exprt = new BibExporter()
+        exprt.exportStub(database.getDB(), "src/mybib")
+    }
+    then 'all book references are being exported to .bib-file',{
+        f = new File("src/mybib.bib")
+        f.delete().shouldBe true
+    }
 }
 
 scenario "file contains as many @book-blocks as there were book references saved to db", {
-    given 'export as .bib'
-    when 'all book references were valid'
-    then 'all saved book references can be found from the file'
+    given 'export as .bib', {
+        book = new Book(
+                "cleancode",
+                "Robert C. Martin",
+                "Clean Code: A Handbook of Agile Software Craftsmanship",
+                "Prentice Hall",
+                "2008")
+        book2 = new Book(
+                "cleancode2",
+                "Robert C. Martin",
+                "Clean Code: A Handbook of Agile Software Craftsmanship",
+                "Prentice Hall",
+                "2008")
+        database = new InMemoryDatabase()
+        database.saveReference(book)
+        database.saveReference(book2)
+    }
+    when 'all book references were valid', {
+        exprt = new BibExporter()
+        exprt.exportStub(database.getDB(), "src/mybib")
+    }
+    then 'all saved book references can be found from the file', {
+        found = 0
+        br = new BufferedReader(new FileReader("src/mybib.bib"));
+        line = "";
+        while ((line = br.readLine()) != null) {
+            if(line.contains("@book")){
+                found++
+            }
+        }
+        br.close();
+        found.shouldBe 2
+    }
 }
