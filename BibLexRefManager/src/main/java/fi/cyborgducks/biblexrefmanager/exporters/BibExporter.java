64be6bfb5965c9ec6c3c8f59cb8b4b1ff40fe859
Leo Kiiski <leo.kiiski@helsinki.fi>
2015-04-12 01:10:25 +0300

    javadoc written

--- BibLexRefManager/src/main/java/fi/cyborgducks/biblexrefmanager/exporters/BibExporter.java
+++ BibLexRefManager/src/main/java/fi/cyborgducks/biblexrefmanager/exporters/BibExporter.java
@@ -9,18 +9,42 @@ import org.jbibtex.BibTeXFormatter;
 
 /**
  *
+ * This class handles exporting references to a .bib file
+ * 
  * @author goalaleo
  */
 public class BibExporter {
-
+    
+/**
+ * 
+ * Takes the references added by the user, and exports them as a .bib file to the chosen location.
+ * When this method is invoked, the program asks for the user to choose a location for the .bib file
+ * to be exported. For testing, use the {@link #exportStub(org.jbibtex.BibTeXDatabase, java.lang.String) } method
+ * 
+ * @param db a BibTexDatabase of the added references
+ * @throws FileNotFoundException
+ * @throws UnsupportedEncodingException
+ * @throws IOException 
+ */
     public static void export(BibTeXDatabase db) throws FileNotFoundException, UnsupportedEncodingException, IOException {
         String path = ChooseFile();
-        if (path == null){
+        if (path == null) {
             return;
         }
         exportStub(db, path);
     }
 
+    /**
+     * 
+     * Handles all the exporting logic apart from choosing a file.
+     * This method can be used for testing.
+     * 
+     * @param db a BibTexDatabase of the added references
+     * @param p the file path where you wish to export to
+     * @throws FileNotFoundException
+     * @throws UnsupportedEncodingException
+     * @throws IOException
+     */
     public static void exportStub(BibTeXDatabase db, String p) throws FileNotFoundException, UnsupportedEncodingException, IOException {
         String path = p;
         if (!path.endsWith(".bib")){
@@ -39,6 +63,12 @@ public class BibExporter {
         }
     }
 
+    /**
+     * 
+     * Creates a pop up window for choosing the export location.
+     * 
+     * @return returns the chosen directory path + file name as an absolute path
+     */
     private static String ChooseFile() {
         JFileChooser chooser = new JFileChooser();
         FileNameExtensionFilter filter = new FileNameExtensionFilter("bib files", "bib");
@@ -50,10 +80,8 @@ public class BibExporter {
 
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             return chooser.getSelectedFile().getAbsolutePath();
-        } else {
-            chooser.cancelSelection();
-        }
-
+        } 
+        
         return null;
     }
 }
