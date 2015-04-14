139b09bdfc02e6d2958497b1b7312d005ca4ef31
Kristian W <kristian.wahlroos@gmail.com>
2015-04-14 19:59:26 +0300

    tests for validation

---
+++ BibLexRefManager/src/test/java/fi/cyborgducks/biblexrefmanager/validators/BookValidatorTest.java
@@ -0,0 +1,111 @@
+/*
+ * Miniprojekti Ohjelmistotuotanto kevät 2015
+ * Helsingin yliopisto Tietojenkäsittelytieteen laitos
+ * Cyborg Ducks 2015 - GNU General Public License, version 3.
+ */
+package fi.cyborgducks.biblexrefmanager.validators;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kride
+ */
+public class BookValidatorTest {
+
+    private Validator bookValidator;
+
+    @Before
+    public void setUp() {
+        bookValidator = new BookValidator();
+    }
+
+    @Test
+    public void initiallyHasNoErrors() {
+        assertEquals(false, bookValidator.hasErrors());
+    }
+
+    @Test
+    public void incorrectKeyRaisesAnError() {
+        bookValidator.isValidOptionalFieldFor("asdaasd");
+        assertEquals("asdaasd was not a correct key for Book reference\n", bookValidator.fullErrors());
+    }
+
+    @Test
+    public void correctKeyNotRaisesAnError() {
+        bookValidator.isValidOptionalFieldFor("month");
+        assertEquals(false, bookValidator.hasErrors());
+    }
+
+    @Test
+    public void emptyKeyNotRaisesAnError() {
+        bookValidator.isValidOptionalFieldFor("");
+        assertEquals(false, bookValidator.hasErrors());
+    }
+
+    @Test
+    public void notValidIfInvalidKey() {
+        bookValidator.isValidParams(new String[]{
+            "c",
+            "Robert C. Martin",
+            "Clean Code: A Handbook of Agile Software Craftsmanship",
+            "Prentice Hall",
+            "2008"
+        });
+        assertEquals(true, bookValidator.hasErrors());
+    }
+
+    @Test
+    public void notValidIfInvalidYear() {
+        bookValidator.isValidParams(new String[]{
+            "cleancode",
+            "Robert C. Martin",
+            "Clean Code: A Handbook of Agile Software Craftsmanship",
+            "Prentice Hall",
+            "asd"
+        });
+        assertEquals(true, bookValidator.hasErrors());
+    }
+
+    @Test
+    public void notValidIfInvalidAuthor() {
+        bookValidator.isValidParams(new String[]{
+            "cleandcode",
+            "Robert C. Martinnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn",
+            "Clean Code: A Handbook of Agile Software Craftsmanship",
+            "Prentice Hall",
+            "2008"
+        });
+        assertEquals(true, bookValidator.hasErrors());
+    }
+
+    @Test
+    public void notValidIfInvalidTitle() {
+        bookValidator.isValidParams(new String[]{
+            "cleancode",
+            "Robert C. Martin",
+            "e",
+            "Prentice Hall",
+            "2008"
+        });
+        assertEquals(true, bookValidator.hasErrors());
+    }
+
+    @Test
+    public void notValidIfInvalidPublisher() {
+        bookValidator.isValidParams(new String[]{
+            "cleancode",
+            "Robert C. Martin",
+            "Clean Code: A Handbook of Agile Software Craftsmanship",
+            "P",
+            "2008"
+        });
+        assertEquals(true, bookValidator.hasErrors());
+    }
+
+}
