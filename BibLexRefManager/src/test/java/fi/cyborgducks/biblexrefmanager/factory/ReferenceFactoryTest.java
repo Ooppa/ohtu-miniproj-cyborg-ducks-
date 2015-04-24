eb23c97415fcbcf26773e5bac555b75b88c62dc2
Kristian Wahlroos <wakr@cs.helsinki.fi>
2015-04-24 13:52:14 +0300

    raising test coverage

--- BibLexRefManager/src/test/java/fi/cyborgducks/biblexrefmanager/factory/ReferenceFactoryTest.java
+++ BibLexRefManager/src/test/java/fi/cyborgducks/biblexrefmanager/factory/ReferenceFactoryTest.java
@@ -2,6 +2,7 @@ package fi.cyborgducks.biblexrefmanager.factory;
 
 import fi.cyborgducks.biblexrefmanager.references.Article;
 import fi.cyborgducks.biblexrefmanager.references.Book;
+import fi.cyborgducks.biblexrefmanager.references.Inproceedings;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -25,16 +26,56 @@ public class ReferenceFactoryTest {
                 }
         );
 
-        
         assertEquals(b.getField(BibTeXEntry.KEY_AUTHOR).toUserString(), "authorValue");
         assertEquals(b.getField(BibTeXEntry.KEY_TITLE).toUserString(), "titleValue");
         assertEquals(b.getField(BibTeXEntry.KEY_PUBLISHER).toUserString(), "publisherValue");
         assertEquals(b.getField(BibTeXEntry.KEY_YEAR).toUserString(), "yearValue");
     }
-    
-   
-    
-    
-    
-    
+
+    @Test
+    public void factoryCreatesArticle() {
+        Article a = ReferenceFactory.createArticle(
+                new String[]{
+                    "key",
+                    "author",
+                    "title",
+                    "journal",
+                    "1999",
+                    "5"
+                }
+        );
+
+        assertTrue(a != null);
+    }
+
+    @Test
+    public void factoryCreatesInproceedings() {
+        Inproceedings i = ReferenceFactory.createInproceedings(
+                new String[]{
+                    "key",
+                    "author",
+                    "title",
+                    "bookTitle",
+                    "1999"
+                }
+        );
+
+        assertTrue(i != null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void factoryThrowsErrorOnBookIfTooFewParams() {
+        ReferenceFactory.createBook(new String[]{});
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void factoryThrowsErrorOnArticleIfTooFewParams() {
+        ReferenceFactory.createArticle(new String[]{});
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void factoryThrowsErrorOnInproceedingsIfTooFewParams() {
+        ReferenceFactory.createInproceedings(new String[]{});
+    }
+
 }
