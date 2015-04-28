d085cb630223316634261654a442b432f38c7a15
goalaleo <leo.kiiski@helsinki.fi>
2015-04-28 16:59:09 +0300

    refaktoroitu exporterit ja importerit pois

--- BibLexRefManager/src/test/java/fi/cyborgducks/biblexrefmanager/helpers/BibTexDataTransferHelperTest.java
+++ BibLexRefManager/src/test/java/fi/cyborgducks/biblexrefmanager/helpers/BibTexDataTransferHelperTest.java
@@ -7,24 +7,128 @@ package fi.cyborgducks.biblexrefmanager.helpers;
 
 import fi.cyborgducks.biblexrefmanager.data.InMemoryDatabase;
 import fi.cyborgducks.biblexrefmanager.references.Book;
+import fi.cyborgducks.biblexrefmanager.ui.FileChooser;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.Stack;
+import org.jbibtex.BibTeXDatabase;
 import org.jbibtex.BibTeXEntry;
 import org.jbibtex.BibTeXParser;
 import org.jbibtex.ObjectResolutionException;
 import org.jbibtex.ParseException;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 /**
  *
  * @author goalaleo
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({FileChooser.class, BibTexDataTransferHelper.class})  // Static.class contains static methods
 public class BibTexDataTransferHelperTest {
 
+    
+     @Test
+    public void importingFromBibFile() throws UnsupportedEncodingException, IOException, FileNotFoundException, ObjectResolutionException, ParseException {
+
+        InMemoryDatabase inMemDB = createDbWithTwoEntriesWhichHaveUmlauts();
+
+        String path1 = "src/mybib";
+        BibTexDataTransferHelper.export(inMemDB.getDB(), path1);
+
+        path1 += ".bib";
+        BibTeXDatabase importedDB = BibTexDataTransferHelper.importFromBib(path1);
+        inMemDB.setDB(importedDB);
+
+        String path2 = "src/mybib2";
+        BibTexDataTransferHelper.export(inMemDB.getDB(), path2);
+
+        path2 += ".bib";
+
+        String bib1 = ScandCharFixer.readFileToString(path1);
+        String bib2 = ScandCharFixer.readFileToString(path2);
+
+        assertEquals(bib1, bib2);
+
+        
+        Files.delete(Paths.get(path1));
+        Files.delete(Paths.get(path2));
+    }
+
+    @Test
+    public void importWorksViaRobot() throws UnsupportedEncodingException, IOException, ObjectResolutionException, ParseException {
+
+        InMemoryDatabase inMemDB = createDbWithTwoEntriesWhichHaveUmlauts();
+
+        String path1 = "src/mybib";
+        BibTexDataTransferHelper.export(inMemDB.getDB(), path1);
+        path1 += ".bib";
+
+        Files.delete(Paths.get(path1));
+    }
+
+    @Test()
+    public void importFromBibReturnsNullIfPathIsNull() throws UnsupportedEncodingException, IOException, ObjectResolutionException, ParseException {
+        PowerMockito.mockStatic(FileChooser.class);
+        BDDMockito.given(FileChooser.chooseFile("Load", "bib")).willReturn(null);
+
+        PowerMockito.mockStatic(BibTexDataTransferHelper.class);
+
+        assertNull(BibTexDataTransferHelper.importFromBib());
+
+        PowerMockito.verifyStatic();
+    }
+
+    
+    @Test
+    public void bracketsMatch() throws UnsupportedEncodingException, IOException {
+        InMemoryDatabase inMemDB = createDbWithTwoEntriesWhichHaveUmlauts();
+
+        String path = "src/mybib";
+        BibTexDataTransferHelper.export(inMemDB.getDB(), path);
+
+        boolean result = bracketsMatchHelper(path);
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void bracketMismatchDetected() throws UnsupportedEncodingException, IOException {
+        InMemoryDatabase inMemDB = new InMemoryDatabase();
+        BibTeXEntry ref = new Book("kikkelis", "Kokkelis Kiiski", "Häläpiti hoi", "Röllituotanto"+"{", "1789");
+        inMemDB.saveReference(ref);
+
+        String path = "src/mybib";
+        BibTexDataTransferHelper.export(inMemDB.getDB(), path);
+
+        boolean result = bracketsMatchHelper(path);
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void exportToNullDoNotCallExportTest() throws UnsupportedEncodingException, IOException {
+        PowerMockito.mockStatic(FileChooser.class);
+        BDDMockito.given(FileChooser.chooseFile("Save", "bib")).willReturn(null);
+
+        BibTeXDatabase mockDB = mock(BibTeXDatabase.class);
+        BibTexDataTransferHelper.export(mockDB);
+
+        PowerMockito.verifyStatic();
+    }
+    
     @Test
     public void scandsGetReplacedWithBibUmlauts() throws UnsupportedEncodingException, IOException {
         InMemoryDatabase inMemDB = createDbWithTwoEntriesWhichHaveUmlauts();
@@ -42,7 +146,7 @@ public class BibTexDataTransferHelperTest {
 
         Files.delete(Paths.get(path));
     }
-
+    
     @Test
     public void bibVersionsOfUmlautsGetReplacedWithRealUmlauts() throws UnsupportedEncodingException, IOException {
         InMemoryDatabase inMemDB = createDbWithTwoEntriesWhichHaveUmlauts();
@@ -64,13 +168,53 @@ public class BibTexDataTransferHelperTest {
         assertEquals(false, fileAsString.contains(bibUmlautO));
         assertEquals(false, fileAsString.contains(bibCapUmlautO));
 
-    }
+    }  
 
     @Test
     public void parseBibTexCreatesNewParser() throws ParseException, ObjectResolutionException, IOException {
         BibTeXParser spyParser = Mockito.spy(new BibTeXParser());
         BibTexDataTransferHelper.importFromBib("pölölö");
         Mockito.verify(spyParser);
+    }
+
+    private boolean bracketsMatchHelper(String path) throws FileNotFoundException, IOException {
+
+        InputStream streamFromFile = new FileInputStream(path+".bib");
+        Scanner scanner = new Scanner(streamFromFile);
+
+        String fileAsString = "";
+
+        while(scanner.hasNextLine()) {
+            String lineInBibFile = scanner.nextLine();
+            fileAsString += lineInBibFile;
+        }
+        scanner.close();
+
+        Stack<Character> stackForBrackets = new Stack();
+
+        for(int i = 0; i<fileAsString.length(); i++) {
+            char c = fileAsString.charAt(i);
+
+            if(c=='{') {
+                stackForBrackets.push(c);
+            }
+
+            if(c=='}') {
+                if(stackForBrackets.empty()) {
+                    stackForBrackets.push(c);
+                    break;
+                } else if(stackForBrackets.peek()=='{') {
+                    stackForBrackets.pop();
+                }
+            }
+
+        }
+        boolean good = stackForBrackets.empty();
+        streamFromFile.close();
+        File file = new File(path+".bib");
+        file.delete();
+
+        return good;
     }
 
     private InMemoryDatabase createDbWithTwoEntriesWhichHaveUmlauts() {
